db6ca1dfe43f11a4e2a84c1ec439a95f8616706a
Otmane Kimdil <okimdil@e2r4p2.1337.ma>
2020-10-17 19:14:36 +0100

    So far so good the code is good as it should be Now we have to go to the camera I added some protection for our ray class and some memory violations So now we are good to move on

--- rays/rays.c
+++ rays/rays.c
@@ -10,7 +10,8 @@ t_ray	*ray(t_vect3f *origin, t_vect3f *direction)
 {
 	t_ray *r;
 
-	r = malloc (sizeof(struct s_ray));
+	if (!(r = malloc(sizeof(struct s_ray))))
+		return NULL;
 	r->origin->x = origin->x;
 	r->origin->y = origin->y;
 	r->origin->z = origin->z;
@@ -20,7 +21,7 @@ t_ray	*ray(t_vect3f *origin, t_vect3f *direction)
 /**
 *	@function => The ray Destoryer function
 *	@return => Nothing just freeing the damn memo
-* */
+**/
 void	ray_destroy(t_ray *ray)
 {
 	free(ray->direction);
@@ -34,29 +35,36 @@ void	ray_destroy(t_ray *ray)
  * @description => Return the oerigin of our ray
  * @param => Pointer to ray struct
  * @returns => ray origin
- * **/
+**/
 t_vect3f	*origin(t_ray *ray)
 {
 	return ray->origin;
 }
 
 /**
- * @description => Return the direction of our ray
- * @param => Pointer to ray struct
- * @returns => ray direction
- * **/
+* @description => Return the direction of our ray
+* @param => Pointer to ray struct
+* @returns => ray direction
+**/
 t_vect3f	*direction(t_ray *ray)
 {
 	return ray->direction;
 }
 
+/**
+* @description => return the position of our vector in the space
+* @param => t :: Real Param to give the direct of our vector
+* 			 origin :: A pointer to the origin of our ray
+* 			 direction :: the direction of our ray
+* @returns => it actually solve this equation :: orig + t * direc
+**/
 t_vect3f	*at(double t, t_vect3f *origin, t_vect3f *direction)
 {
 	t_vect3f *result;
 	t_vect3f *tmp;
 
-	tmp = vec3f_multi (direction, t);
-	result = vec3f_add (origin, tmp);
+	tmp = vec3f_multi(direction, t);
+	result = vec3f_add(origin, tmp);
 	free(tmp);
 	return (result);
 }
--- vec3f/vec3f.c
+++ vec3f/vec3f.c
@@ -5,117 +5,117 @@
 
 t_vect3f *vec3f(float x, float y, float z)
 {
-    t_vect3f *v;
-    
-    v = malloc (sizeof(struct s_vect3f));
-    v->x = x;
-    v->y = y;
-    v->z = z;
-    return (v);
+	t_vect3f *v;
+
+	if (!(v = malloc(sizeof(struct s_vect3f))))
+		return NULL;
+	v->x = x;
+	v->y = y;
+	v->z = z;
+	return (v);
 }
 
 void vec3f_destroy (t_vect3f *vec)
 {
-    free (vec);
+	free (vec);
 }
 
 t_vect3f *vec3f_add(t_vect3f *t1, t_vect3f *t2)
 {
-    t_vect3f *sum;
+	t_vect3f *sum;
 
-    sum = vec3f(0, 0, 0);
-    sum->x = t1->x + t2->x;
-    sum->y = t1->y + t2->y;
-    sum->z = t1->z + t2->z;
-    return (sum);
+	sum = vec3f(0, 0, 0);
+	sum->x = t1->x + t2->x;
+	sum->y = t1->y + t2->y;
+	sum->z = t1->z + t2->z;
+	return (sum);
 }
 
 t_vect3f *vec3f_sub(t_vect3f *v1, t_vect3f *v2)
 {
-    t_vect3f *diff;
+	t_vect3f *diff;
 
-    diff = vec3f (0, 0, 0);
-    diff->x = v1->x - v2->x;
-    diff->y = v1->y - v2->y;
-    diff->z = v1->z - v2->z;
-    return (diff);
+	diff = vec3f (0, 0, 0);
+	diff->x = v1->x - v2->x;
+	diff->y = v1->y - v2->y;
+	diff->z = v1->z - v2->z;
+	return (diff);
 }
 
 t_vect3f *vec3f_neg(t_vect3f *v)
 {
-    t_vect3f *neg;
+	t_vect3f *neg;
 
-    neg = vec3f (0,0, 0);
-    neg->x = v->x * -1;
-    neg->y = v->y * -1;
-    neg->z = v->z * -1;
-    return (neg);
+	neg = vec3f (0,0, 0);
+	neg->x = v->x * -1;
+	neg->y = v->y * -1;
+	neg->z = v->z * -1;
+	return (neg);
 }
 
 t_vect3f *vec3f_multi(t_vect3f *t, float s)
 {
-    t_vect3f *r;
+	t_vect3f *r;
 
-    r = vec3f (0, 0, 0);
-    r->x = t->x * s;
-    r->y = t->y * s;
-    r->z = t->z * s;
-    return (r);
+	r = vec3f (0, 0, 0);
+	r->x = t->x * s;
+	r->y = t->y * s;
+	r->z = t->z * s;
+	return (r);
 }
 
 t_vect3f *vec3f_div(t_vect3f *t, float d)
 {
-    t_vect3f *r;
+	t_vect3f *r;
 
-    r = vec3f (0, 0, 0);
-    r->x = t->x / d;
-    r->y = t->y / d;
-    r->z = t->z / d;
-    return (r);
+	r = vec3f (0, 0, 0);
+	r->x = t->x / d;
+	r->y = t->y / d;
+	r->z = t->z / d;
+	return (r);
 }
 
 float vec3f_mag(t_vect3f *vec)
 {
-    double m;
+	double m;
 
-    m = 0;
-    m = sqrt(pow(vec->x, 2) + pow(vec->y, 2) + pow(vec->z, 2));
-
-    return (m);
+	m = 0;
+	m = sqrt(pow(vec->x, 2) + pow(vec->y, 2) + pow(vec->z, 2));
+	return (m);
 }
 
 t_vect3f *vec3f_norm(t_vect3f *vec)
 {
-    t_vect3f *norm;
-    double mag;
+	t_vect3f *norm;
+	double mag;
 
-    norm = vec3f (0, 0, 0);
-    mag = vec3f_mag(vec);
-    norm->x = vec->x / mag;
-    norm->y = vec->y / mag;
-    norm->z = vec->z / mag;
-    return norm;
+	norm = vec3f (0, 0, 0);
+	mag = vec3f_mag(vec);
+	norm->x = vec->x / mag;
+	norm->y = vec->y / mag;
+	norm->z = vec->z / mag;
+	return norm;
 }
 
 float vec3f_dotproduct(t_vect3f *v1, t_vect3f *v2)
 {
-    return ((v1->x * v2->x) + (v1->y * v2->y) + (v1->z * v2->z));
+	return ((v1->x * v2->x) + (v1->y * v2->y) + (v1->z * v2->z));
 }
 
 t_vect3f *vec3f_crossproduct(t_vect3f *v1, t_vect3f *v2)
 {
-    t_vect3f *cpv;
+	t_vect3f *cpv;
 
-    cpv = vec3f (0, 0, 0);
-    cpv->x = (v1->x * v2->z) - (v1->z * v2->y);
-    cpv->y = (v1->z * v2->x) - (v1->x * v2->z);
-    cpv->z = (v1->x * v2->y) - (v1->y * v2->x);
-    return (cpv);
+	cpv = vec3f (0, 0, 0);
+	cpv->x = (v1->x * v2->z) - (v1->z * v2->y);
+	cpv->y = (v1->z * v2->x) - (v1->x * v2->z);
+	cpv->z = (v1->x * v2->y) - (v1->y * v2->x);
+	return (cpv);
 }
 
 void vec3f_copy (t_vect3f *src, t_vect3f *dst)
 {
-    dst->x = src->x;
-    dst->y = src->y;
-    dst->z = src->z;
+	dst->x = src->x;
+	dst->y = src->y;
+	dst->z = src->z;
 }
