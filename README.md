28f9521f0357029dda5aaf61c3aa2fb8807063cd
Khalil Hafni <khafni@e2r2p6.1337.ma>
2020-10-28 12:27:14 +0100

    corrected shitty merge

--- README.md
+++
@@ -1,2 +0,0 @@
-# MINIRT (Raytracing in C)
-
--- hittable/hittable.c
+++
@@ -1,8 +0,0 @@
-#include "hittable.h"
-
-void set_face_normal(t_ray *r, t_s_vect3f outward_normal, t_hit_record *hr)
-{
-    hr->front_face = s_vec3f_dotproduct(*r->direction, outward_normal) < 0;
-    hr->normal = hr->front_face ? outward_normal : outward_normal;
-}
-
