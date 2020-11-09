581b5980bce184cbc79a6116d711bc65afe5a905
Khalil Hafni <khafni@e2r2p6.1337.ma>
2020-11-09 13:48:48 +0100

    o

--- bpm.c
+++ bpm.c
@@ -1,24 +1,39 @@
 #include "bpm.h"
 #include "bpm.h"
 
-
-void write_bmp (char *filename, char *rgb, int width, int height)
+void write_bmp(char *filename, t_bmp rgb, int width, int height)
 {
-    char tag[2];
-    int header[13];
-    int padding;
-    char *bitmap;
+    char tag[] = {'B', 'M'};
+    int header[] = {
+        0, 0, 0x36, 0x28, width, height, 0x180001,
+        0, 0, 0, 0, 0, 0};
+    char *bitmap = (char*) rgb.pixels;
+    int fd = open("bm1.bmp", O_CREAT | O_WRONLY, S_IRWXU);
+    write(fd, &tag, sizeof(tag));
+    write(fd, &header, sizeof(header));
+    write(fd, &bitmap, sizeof(bitmap));
+}
 
-    padding = 0;
-    if (((width * 3) % 4) != 0)
-        padding = width;
-    bitmap = malloc (sizeof(char) * ((width * height) + padding));
+t_bmp create_pixels_array(int width, int height)
+{
+    t_bmp bmp;
+
+    bmp.width = width;
+    bmp.height = height;
+    bmp.pixels = malloc(sizeof(uint32_t) * (width * height));
+    return (bmp);
+}
+
+
+void set_bmp_pixel (int x, int y, t_trgb value)
+{
+    
 }
 
 int main()
 {
 
-    int width = 4;
+    int width = 3;
     int height = 2;
     char tag[] = {'B', 'M'};
     int header[] = {
@@ -37,14 +52,13 @@ int main()
         0x41, // Green
         0xef, // Red
 
-        0x35, // Blue
-        0x41, // Green
-        0xef, // Red
-
+        0x00, // Padding
+        0x00, // Padding
+        0x00, // Padding
 
         255, // Blue
-        0, // Green
-        0, // Red
+        0,   // Green
+        0,   // Red
 
         0x35, // Blue
         0x41, // Green
@@ -54,13 +68,9 @@ int main()
         0x41, // Green
         0xef, // Red
 
-        0x35, // Blue
-        0x41, // Green
-        0xef // Red
-
-        // 0x00, // Padding
-        // 0x00, // Padding
-        // 0x00,  // Padding
+        0x00, // Padding
+        0x00, // Padding
+        0x00, // Padding
         // 0x00, // Padding
     };
     int fd = open("bm1.bmp", O_CREAT | O_WRONLY, S_IRWXU);
--- bpm.h
+++ bpm.h
@@ -1,20 +1,26 @@
 #ifndef BPM_H
 #define BPM_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include "image/image.h"
+
 typedef struct s_bmp t_bmp;
 
 struct s_bmp
 {
     int width;
     int height;
-    char *rgb;
+    int padding;
+    uint32_t *pixels;
 };
+void write_bmp(char *filename, t_bmp rgb, int width, int height);
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 
+t_bmp create_pixels_array (int width, int height);
 
 
 #endif
