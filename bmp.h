a17380a681092dee425ac2e70a38096c36c28d4e
Khalil Hafni <khafni@e2r2p6.1337.ma>
2020-11-10 14:28:49 +0100

    complete nonsense still trying to write into bmp

--- bmp.h
+++ bmp.h
@@ -14,13 +14,17 @@ struct s_bmp
 {
     int width;
     int height;
-    int padding;
-    uint32_t *pixels;
+    int padded_width;
+    int bitmap_size;
+    char *pixels;
 };
 void write_bmp(char *filename, t_bmp rgb, int width, int height);
 
 
 t_bmp create_pixels_array (int width, int height);
+
+void set_bmp_pixel (t_bmp *bmp ,int x, int y, t_trgb value);
+void write_bmp1(char *filename, int length, int width);
 
 
 #endif
