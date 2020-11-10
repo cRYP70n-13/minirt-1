73d806ed0a87a78bf18a957aa4921ff09e94fa43
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-10 17:12:39 +0100

    Formating and start understanding WTF is going on there

--- camera.h
+++ camera.h
@@ -1,5 +1,5 @@
 #ifndef CAMERA_H
-#define CAMERA_H
+# define CAMERA_H
 
 # include "vec3f/vec3f.h"
 # include "rays/rays.h"
@@ -9,15 +9,13 @@ typedef struct s_camera t_camera;
 
 struct s_camera
 {
-    t_s_vect3f origin;    
-    t_s_vect3f lower_left_corner;
-    t_s_vect3f horizontal;
-    t_s_vect3f vertical;
+	t_s_vect3f origin;    
+	t_s_vect3f lower_left_corner;
+	t_s_vect3f horizontal;
+	t_s_vect3f vertical;
 };
 
-t_camera camera ();
-
-t_ray get_ray (t_camera cam, double u, double v);
-
+t_camera camera();
+t_ray get_ray(t_camera cam, double u, double v);
 
 #endif
--- generic_array/garray.c
+++ generic_array/garray.c
@@ -2,11 +2,11 @@
 
 t_array empty_array_create(t_destructor obj_des)
 {
-    
+    // TODO: Wait for pizzaboi to explain me WTF is going on here
 }
+
 t_array empty_array_create_size(int size, t_destructor obj_des);
 void array_destroy(t_array *a);
-
 void *array_get(t_array a, int index);
 void array_set(t_array a, int index, void *obj);
 void array_add(t_array a, void *obj);
